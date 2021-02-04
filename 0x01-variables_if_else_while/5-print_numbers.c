d5a51e255a059af2662965932de8c77c6a124642
SeanFrechette <sea.frechette@gmail.com>
2021-02-04 21:04:34 +0000

    Added 5-print_numbers file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		printf("%d", x);
+	}
+	putchar(10);
+	return (0);
+}
