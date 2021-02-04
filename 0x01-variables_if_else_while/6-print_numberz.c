8d59ad1934bad68e34cbba1c482893d02af1fc3a
SeanFrechette <sea.frechette@gmail.com>
2021-02-04 21:09:34 +0000

    Added 6-print_numberz file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48 ; x <= 57 ; x++)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
