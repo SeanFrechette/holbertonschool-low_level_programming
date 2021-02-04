4f38d9ed379ba1bdb6a274dba75d848fbd64935b
SeanFrechette <sea.frechette@gmail.com>
2021-02-04 22:05:35 +0000

    Added 9-print_comb file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0' ; x <= '9' ; x++)
+	{
+		putchar(x);
+		if (x != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
