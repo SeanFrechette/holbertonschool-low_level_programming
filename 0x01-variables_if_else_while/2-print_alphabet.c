cba1a91b9ed5c92bafc82f99dfbb7e8b5fdddfed
SeanFrechette <sea.frechette@gmail.com>
2021-02-04 20:43:30 +0000

    Added 2-print_alphabet file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints lowercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a' ; x <= 'z' ; x++)
+	{
+		putchar(x);
+	}
+	putchar(10);
+	return (0);
+}
+
+
