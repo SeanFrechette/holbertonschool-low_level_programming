9c2c45dbfb2a67b358472867ef4c32189bc73eff
SeanFrechette <sea.frechette@gmail.com>
2021-02-04 19:38:07 +0000

    Fixed 5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,8 @@
-#include <studio.h>
-
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: - returns 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
