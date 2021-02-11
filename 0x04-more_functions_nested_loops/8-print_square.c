872afcc7cd0175d194dd0aae5482514970188dcb
SeanFrechette <sea.frechette@gmail.com>
2021-02-11 23:26:28 +0000

    Added file 8 to 0x04 and updated holberton header

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_square - prints an NxN square
+ * @size: size of square
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int x, y = 0;
+
+	for (y = 0 ; y < size ; y++)
+	{
+		for (x = 0 ; x < size ; x++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
