be54c5bfb0956239eac79a7349bc2e18e6d6b5a7
SeanFrechette <sea.frechette@gmail.com>
2021-02-16 21:23:44 +0000

    Fixed file 2

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strcpy - copies the string, pointed to by src, to dest
+ * @dest: Buffer pointer
+ * @src: String input
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+
+}
