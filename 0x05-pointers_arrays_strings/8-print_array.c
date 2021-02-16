8a8069cdf5f38dc8a16af193cda20cb4ebd649a6
SeanFrechette <sea.frechette@gmail.com>
2021-02-16 20:49:28 +0000

    Fixed file 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,11 +7,15 @@
  */
 void print_array(int *a, int n)
 {
-	int len = sizeof(a) / sizeof(a[0]);
+	int i;
 
-	for (; n < len; n++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[n]);
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
 	}
-	printf('\n');
+	printf("\n");
 }
