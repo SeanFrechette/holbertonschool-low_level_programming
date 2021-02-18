7d6c038ecad2763e26752ec10db0d94e8c2a09b1
SeanFrechette <sea.frechette@gmail.com>
2021-02-18 23:27:39 +0000

    Added file 5 to 0x06

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * string_toupper - converts lowercase chars to uppercase
+ * @a: String input
+ * Return: uppercase string
+ */
+char *string_toupper(char *a)
+{
+	int i = 0;
+
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			a[i] = a[i] - 32;
+		}
+		i++;
+	}
+	return (a);
+}
