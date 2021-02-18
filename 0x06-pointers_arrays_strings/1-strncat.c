6379f795d897bff4fffdddfed30a098dfc54f8c6
SeanFrechette <sea.frechette@gmail.com>
2021-02-18 18:56:21 +0000

    Added file 1 to 0x06

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * _strncat - appends n bytes from src
+ * @dest: main String
+ * @src: string to append to dest
+ * @n: prints at most n bytes from src
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, x = 0;
+ 
+	for (i = 0; dest[i] != '\0'; i++)
+	{}
+	while (src[x] != '\0')
+	{
+		if (x < n)
+		{
+			dest[i + x] = src[x];
+		}
+		x++;
+	}
+	return (dest);
+}
