c2bd3d70729142434a044e5dabeca3ae4383beed
SeanFrechette <sea.frechette@gmail.com>
2021-02-18 19:34:54 +0000

    Added file 2 to 0x06

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strncpy - copies a string, src to dest
+ * @dest: return copied string
+ * @src: string to be copied
+ * @n: amount of chars to copy
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && src[x] != '0'; x++)
+	{
+		dest[x] = src[x];
+	}
+	while (x < n)
+	{
+		dest[x] = '\0'
+		x++;
+	}
+	return (dest);
+}
