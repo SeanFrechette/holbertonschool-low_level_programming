3ecf0b0effec8816ab666529e68f6e7cd9735769
SeanFrechette <sea.frechette@gmail.com>
2021-02-19 01:56:07 +0000

    Added file 6 to 0x06

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,16 +7,17 @@
 char *leet(char *a)
 {
 	int x = 0, y = 0;
-	char alpha[] = "aeotlAEOTL";
+	char alpha[] = "aeotl";
+	char upperAlpha[] = "AEOTL";
 	char num[] = "43071"
 
 	while (a[x] != '\0')
 	{
 		for (y = 0; alpha[y] != '\0'; y++)
 		{
-			if (a[x] == alpha[y])
+			if (a[x] == alpha[y] || a[x] == upperAlpha[y])
 			{
-				a[x] = alpha[y];
+				a[x] = num[y];
 			}
 		}
 		x++;
