0e61e66f20c3b9298505ae8c374ecb9cda7f1cb6
SeanFrechette <sea.frechette@gmail.com>
2021-02-19 00:26:34 +0000

    Added file 6 to 0x06

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * cap_string - capitalize words of a string
+ * @a: string input
+ * Return: a
+ */
+char *cap_string(char *a)
+{
+	int i = 0, x = 0;
+	char *sep = " \t \n,;.!?\"\(\)\{\}";
+	
+	while (a[i] != '\0')
+	{
+		if (a[i] >= 'a' && a[i] <= 'z')
+		{
+			while (sep[x] != '\0')
+			{
+				if (sep[x] == a[i - 1])
+				{
+					a[i] = a[i] - 32;
+				}
+				x++;
+			}
+		}
+		i++;
+	}
+	return (a);
+}
