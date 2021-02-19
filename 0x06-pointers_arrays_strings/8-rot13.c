c8cd5dc0b4cf3a23b890121fb250c3f342d9eb0c
SeanFrechette <sea.frechette@gmail.com>
2021-02-19 02:04:48 +0000

    Added file 8 to 0x06

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+   * rot13 - check the code for Holberton School students.
+    *@n: the variable
+     *Return: n variable
+      */
+char *rot13(char *a)
+{
+	int x, y;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (x = 0; a[x] != '\0'; x++)
+	{
+		for (y = 0; alpha[y] != '\0'; y++)
+		{
+			if (a[x] == alpha[y])
+			{
+				a[x] = rot[y];
+				break;
+			}
+		}
+	}
+	return (n);
+}
