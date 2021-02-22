2801f56ea53b25e0ead3430169ae3a11251f2ad4
SeanFrechette <sea.frechette@gmail.com>
2021-02-22 23:24:34 +0000

    Added file 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * _strpbrk - searches a string for a set of bytes
+ * @s: input
+ * @accept: input
+ * Return: return byte in s that matches accept, or null if no byte exists
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	unsigned int x, y;
+
+	for (x = 0; s[x]; x++)
+	{
+		for (y = 0; accept[y]; y++)
+		{
+			if (s[x] == accept[y])
+				break:
+		}
+		if (accept[y] != '\0')
+			return (s + x)
+	}
+	return ('\0');
+}
