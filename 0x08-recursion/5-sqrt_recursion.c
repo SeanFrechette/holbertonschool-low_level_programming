be979c1e510ef34db9f4dd48edae6455ee389743
Sean Frechette <sea.frechette@gmail.com>
2021-02-24 19:15:29 -0500

    Fixed file 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,20 @@
 #include "holberton.h"
 /**
+ * sqrtHelper - executes math for _sqrt_recursion
+ * @n: number to sqrt
+ * @i: iterator
+ * Return: returns the natural sqrt of n, or -1 if one doesn't exist
+ */
+int sqrtHelper(int n, int i)
+{
+    if (n < (i * i))
+        return (-1);
+    else if (n == (i * i))
+        return (i);
+    else
+        return (sqrtHelper(n, i + 1));
+}
+/**
  * _sqrt_recursion - returns the square root of a number
  * @n: number
  * Return: the natural square root, or -1 if n doesn't have one
@@ -9,14 +24,4 @@ int _sqrt_recursion(int n)
     if (n < 0)
         return (-1);
     return (sqrtHelper(n, 1));
-}
-
-int sqrtHelper(int n, int i)
-{
-    if (n < (i * i))
-        return (-1);
-    else if (n == (i * i))
-        return (i);
-    else
-        return (sqrtHelper(n, i + 1));
 }
