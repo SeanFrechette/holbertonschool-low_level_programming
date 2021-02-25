257a649caf72446ae8b545278c711a448585c368
Sean Frechette <sea.frechette@gmail.com>
2021-02-24 21:05:43 -0500

    Added file 6 to 0x08

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+int is_prime(int n, int i)
+{
+    if (n == i)
+        return (1);
+    if (n % i == 0)
+        return (0);
+    return (is_prime(n, i + 1));
+}
+/**
+ * is_prime_number - finds if n is prime
+ * @n: number
+ * Return: returns 1 if n is prime, or 0 if otherwise
+ */
+int is_prime_number(int n)
+{
+    if (n < 2 || (n % 2 == 0 && n != 2))
+        return (0);
+    return (is_prime(n, 2));
+}
