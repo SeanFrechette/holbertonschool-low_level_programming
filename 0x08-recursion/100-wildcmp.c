80e020985a813609699c60f0234c26bca9a4329d
Sean Frechette <sea.frechette@gmail.com>
2021-02-24 22:18:46 -0500

    Added file 100 for 0x08

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * wildcmp - compares two strings
+ * @s1: string input 1
+ * @s2: string input 2
+ * Return: 1 if the strings are identical, 0 otherwise
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+}
