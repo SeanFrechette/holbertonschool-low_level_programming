c7fcc5be568d60cb63992cd843e9676d9c0cc59e
Sean Frechette <sea.frechette@gmail.com>
2021-02-27 18:03:28 -0500

    Fixed file 4 for 0x0A

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,23 +9,23 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
+	int i, sum = 0;
 
 	if (argc <= 1)
-	{
 		printf("0\n");
-	}
-	for (i = 1; i < argc; i++){
-		if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+	else
+	{
+		for (i = 1; i < argc; i++)
 		{
-			printf("Error\n");
-			return (1);
-		}
-		else
-		{
-			printf("%d\n", atoi(argv[1]) + atoi(argv[2]));
-			break;
+			if (!(*argv[i] >= '0' && *argv[i] <= '9'))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+				sum += atoi(argv[i]);
 		}
 	}
+	printf("%i\n", sum);
 	return (0);
 }
