2b809ff3d9bdbf451d0465109ebd90ea20d4dd21
Sean Frechette <sea.frechette@gmail.com>
2021-02-27 16:19:45 -0500

    Added file 1 to 0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - print name of program
+ * @argc: array length
+ * @argv: array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	*argv = *argv;
+
+	printf("%i\n", argc);
+	return (0);
+}
