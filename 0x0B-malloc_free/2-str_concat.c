70eff793fc53aaadafde4e8b70008a7cdb799285
Sean Frechette <sea.frechette@gmail.com>
2021-03-02 08:17:32 -0500

    Added file 2 to 0x0B

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * str_concat - concatenates two strings
+ * @s1: string input 1
+ * @s2: string input 2
+ * Return: a pointer to the newly allocated space in memory, which contains s1 followed by s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int len1, len2, i;
+
+	if (s1 == NULL)
+	{
+		len1 = 0;
+	}
+	else if (s2 == NULL)
+	{
+		len2 = 0;
+	}
+
+	for (len1 = 1; s1[len1]; len1++)
+	{}
+	for (len2 = 1; s2[len2]; len2++)
+	{}
+
+	char *arr = (char*)malloc((len1 + len2) * sizeof(char));
+
+	for (i = 0; i <= len1; i++)
+		arr[i] = s1[i];
+	for (i = 0; i <= len2; i++)
+		arr[i + len1] = s2[i];
+	return (arr);
+}
