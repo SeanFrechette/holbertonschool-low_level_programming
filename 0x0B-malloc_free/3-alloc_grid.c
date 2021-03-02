fa47ea18604e62585cc840cc161bae0e97f835b2
Sean Frechette <sea.frechette@gmail.com>
2021-03-02 14:33:45 -0500

    Fixed file 3 in 0x0B

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a 2 dimensional array of ints
+ * @width: x input
+ * @height: y input
+ * Return: returns a pointer to a 2 dimensional array
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int **arr;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = (int **)calloc(height, sizeof(int *));
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = calloc(width, sizeof(int));
+	}
+	return (arr);
+}
