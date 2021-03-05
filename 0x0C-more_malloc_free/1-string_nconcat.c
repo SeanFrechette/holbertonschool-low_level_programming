7f5608680aefbca64629e3b187a5cf65f2fd92a8
Sean Frechette <sea.frechette@gmail.com>
2021-03-04 19:29:46 -0500

    Fixed formating for files

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,6 +11,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1 = 0, len2 = 0, i;
 	char *arr;
+
 	if (!s1)
 		s1 = "";
 	if (!s2)
@@ -23,6 +24,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	arr = malloc((len1 + len2 + 1) * sizeof(char));
 	if (!arr)
 		return (NULL);
+
 	for (i = 0; i <= len1; i++)
 			arr[i] = s1[i];
 	for (i = 0; i <= n; i++)
