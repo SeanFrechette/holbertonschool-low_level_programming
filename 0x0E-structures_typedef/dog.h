b0bd4c7d77002811f5f34d48c5ff925e8808ab47
Sean Frechette <sea.frechette@gmail.com>
2021-03-08 13:45:20 -0500

    Added file to 0x0E

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,12 @@
+/**
+ * struct dog - stores name, age, and owner of a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
