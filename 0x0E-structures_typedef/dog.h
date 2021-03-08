c1a42139674c8a480e4e606c01d7cae222eb4d83
Sean Frechette <sea.frechette@gmail.com>
2021-03-08 13:47:52 -0500

    Fixed formating for file in 0x0E

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - stores name, age, and owner of a dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif
