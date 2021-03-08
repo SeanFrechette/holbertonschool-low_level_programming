e37b2dd3d90ea6d9ca1a20775a4d7e11da52ce23
Sean Frechette <sea.frechette@gmail.com>
2021-03-08 15:17:18 -0500

    Fixed file 4 in 0x0E

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,6 +28,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; i < namelen; i++)
 		dog->name[i] = name[i];
+/* store age */
+	dog->age = age;
 /* store owner */
 	for (i = 0; i < name[ownerlen]; i++)
 		ownerlen++;
