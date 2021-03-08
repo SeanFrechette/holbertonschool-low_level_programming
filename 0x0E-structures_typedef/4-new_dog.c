18ae59588f4c7a7749977b0b5747bbb603dedaa5
Sean Frechette <sea.frechette@gmail.com>
2021-03-08 15:12:10 -0500

    Added file 4 to 0x0E and updated dog header

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,44 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * new_dog - creates a new dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ * 
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int namelen, ownerlen, i;
+	dog_t *dog;
+/* define dog */
+	if (!name || !owner)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (!dog)
+		return (NULL);
+/* store name */
+	for (i = 0; i < name[namelen]; i++)
+		namelen++;
+	dog->name = malloc(namelen * sizeof(char));
+	if (!(dog->name))
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < namelen; i++)
+		dog->name[i] = name[i];
+/* store owner */
+	for (i = 0; i < name[ownerlen]; i++)
+		ownerlen++;
+	dog->owner = malloc(ownerlen * sizeof(char));
+	if (!(dog->owner))
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ownerlen; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
+}
