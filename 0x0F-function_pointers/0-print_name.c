7fc74707936735b557c909299c6735671559ca1e
Sean Frechette <sea.frechette@gmail.com>
2021-03-10 15:18:59 -0500

    Added file 0 to 0x0F

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - selects which function to parse "name" through
+ * @name: string input
+ * @f: function to parse "name"
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
