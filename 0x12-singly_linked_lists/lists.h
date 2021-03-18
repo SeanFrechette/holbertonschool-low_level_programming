fe44f87bdc938620703a290ca08bfba4222185c3
Sean Frechette <sea.frechette@gmail.com>
2021-03-18 16:34:18 -0400

    [Fix] formating

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LIST_H
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -16,3 +18,5 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+#endif
