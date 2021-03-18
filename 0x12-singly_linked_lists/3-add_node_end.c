cd49129679b3a128b01e0f4fd601b5a40dcb4843
Sean Frechette <sea.frechette@gmail.com>
2021-03-18 18:33:56 -0400

    [Add] file 3

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *list;
+	list_t *temp = *head;
+	int len;
+
+	if (!head)
+		return (NULL);
+
+	list = malloc(sizeof(list_t));
+	if (!list)
+		return (NULL);
+
+	list->next = NULL;
+	for (len = 0; str[len]; len++)
+		;
+	list->str = strdup(str);
+	list->len = len;
+
+	if (!*head)
+	{
+		*head = list;
+		return (list);
+	}
+	
+	while (temp->next)
+		temp = temp->next;
+
+	temp->next = list;
+
+	return (*head);
+}
