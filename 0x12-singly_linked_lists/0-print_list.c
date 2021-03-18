bf234d39e542a8dfb89ceffcc178ebb690983d92
Sean Frechette <sea.frechette@gmail.com>
2021-03-18 16:27:11 -0400

    Created 0x12 dir and readme file

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_list - prints elements of list_t
+ * @h: pointer to list_t
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	char *str;
+	unsigned int len;
+	size_t count = 0;
+
+	while (h)
+	{
+		str = h->str;
+		len = h->len;
+		if (str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s", len, str);
+		
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
