f03d8c4dadcc29a64ff00ebff59e9d1388d3df86
Sean Frechette <sea.frechette@gmail.com>
2021-03-23 17:22:15 -0400

    [Add] Files 5 and 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * pop_listint - delete the head node of a list
+ * @head: address to list
+ * Return: head nodes data, 0 if empty
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *list;
+	int num;
+
+	if (!head)
+		return (0);
+
+	num = (*head)->n;
+	list = (*head)->next;
+	free(*head);
+	*head = list;
+	
+	return (num);
+}
