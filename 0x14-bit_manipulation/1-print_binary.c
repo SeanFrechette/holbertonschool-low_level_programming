a756ae6825fc36836c08289b83254c73047acc4b
Sean Frechette <sea.frechette@gmail.com>
2021-03-25 19:01:14 -0400

    [Add] File 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_binary_rec - recursively loop through n
+ * @n: decimal input
+ */
+void print_binary_rec(unsigned long int n)
+{
+	if (n != 0)	
+	{
+		print_binary_rec(n >> 1);
+		putchar((n & 1) ? '1' : '0');
+	}
+}
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: decimal input
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+		putchar('0');
+	else
+		print_binary_rec(n);
+}
