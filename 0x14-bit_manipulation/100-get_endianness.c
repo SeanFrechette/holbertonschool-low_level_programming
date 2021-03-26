04ca6df7d5d1cc640af657417068677fd4f1ea2f
Sean Frechette <sea.frechette@gmail.com>
2021-03-26 09:38:13 -0400

    [Add] File 100

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,4 +1,18 @@
+#include "holberton.h"
+/**
+ * get_endianness - checks the endianness
+ * 0 if big endian, 1 if little endian
+ */
 int get_endianness(void)
 {
-	
+	int num = 1;
+
+	if (*(char *)&num == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
