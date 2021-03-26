7aae77a8cce63424495dd555ce640ea0c4040459
Sean Frechette <sea.frechette@gmail.com>
2021-03-25 20:16:10 -0400

    [Fix] Formating

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,12 +9,10 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	if (index > 63 || !n)
 		return (-1);
+
+	if (n & (1 << index))
+		return (1);
 	else
-	{
-		if (n & (1 << index))
-			return (1);
-		else
-			return (0);
-	}
+		return (0);
 	return (-1);
 }
