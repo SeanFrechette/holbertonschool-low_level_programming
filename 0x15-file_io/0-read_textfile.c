f0e4e5e9adc81c2b6625727246f1777ed9a1e42b
Sean Frechette <sea.frechette@gmail.com>
2021-03-30 17:43:05 -0400

    [Add] Readme File, holberton header, and File 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * read_textfile - reads and prints a text file
+ * @filename: name of file to read
+ * @letters: num of letters that should be read and print
+ * Return: the number of letters that could be read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t rd, wr;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_WRONLY, 0600);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+	{
+		close(fd);
+		return (0);
+	}
+
+	rd = read(fd, buf, letters);
+	if (rd == -1)
+	{
+		close(fd);
+		return (0);
+	}
+	wr = write(STDOUT_FILENO, buf, rd);
+	close(fd);
+	return (wr);
+}
