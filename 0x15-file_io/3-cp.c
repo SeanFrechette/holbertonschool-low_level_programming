8cea2b778f080a8f52bf0752c529b6c6c4d4d783
Sean Frechette <sea.frechette@gmail.com>
2021-03-30 20:43:21 -0400

    [Add] File 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -26,14 +26,14 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(99);
 	}
 	rd = read(file_from, buf, 1024);
 	wr = write(file_to, buf, rd);
 	if (rd != wr)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
 		exit(99);
 	}
 	if (close(file_from) == -1)
